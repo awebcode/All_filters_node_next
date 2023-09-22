b32d989b143818bcb77246425bc8119711b1df22
Md-Asikur <asikurrahaman997@gamil.com>
2023-09-22 23:22:51 +0600

    x

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,25 +4,28 @@ const Product = require("../models/Product");
 
 const productController = {};
 productController.createProduct = async (req, res) => {
-
- try {
-     const newProuct = await Product.create(req.body);
-      res.status(201).json({ message: "Product created",newProuct });
- } catch (error) {
-    res.status(500).json({message:"Internal server error"})
- }
-
-}
+  try {
+    const newProuct = await Product.create(req.body);
+    res.status(201).json({ message: "Product created", newProuct });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 productController.getFilteredProducts = async (req, res) => {
   try {
     const filters = {};
     const { search, category, minPrice, maxPrice, page, limit, sortBy } = req.query;
 
-    // Search by name
-    if (search) {
-      filters.name = { $regex: new RegExp(search, "i") };
+    // Search by 
+    if (req.query.search) {
+      const searchRegex = new RegExp(req.query.search, "i");
+      filters.$or = [
+        { name: { $regex: searchRegex } },
+        { category: { $regex: searchRegex } },
+        
+      ];
     }
-
+    //also you should note that regex only work on string not number like price,rating
     // Filter by category
     if (category) {
       filters.category = category;
